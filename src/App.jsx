fcbe96508acc741a2cea60fbd7d52a063d6f6dcd
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-08-08 01:43:11 -0500

    Protipado de la sección de Nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import { Route, Routes } from 'react-router'
 import Home from './components/Home'
+import Nosotros from './components/Nosotros'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/nosotros' element={<Home/>}/>
+          <Route path='/nosotros' element={<Nosotros />} />
         </Routes>
       </main>
     </div>
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import '../styles/Nosotros.css'
+import photo1 from '../assets/photo_1.jpg'
+import photo2 from '../assets/photo_2.jpg'
+import photo3 from '../assets/photo_3.jpg'
 
 const Nosotros = () => {
-  return (
-    <div>Nosotros</div>
-  )
+    return (
+        <div className="Nosotros">
+            <h2 className='Nosotros_title'>¿Quiénes somos?</h2>
+            <div className="Nosotros_container">
+                <div className="Nosotros_img">
+                    <img src={photo1} alt="Dahiana Arango" />
+                </div>
+                <div className="Nosotros_main">
+                    <h3 className="Nosotros_name">Dahiana Arango Bedoya - <span><i>Estudiante Ingeniería Civil</i></span></h3>
+                    <p className='Nosotros_paragraph'>Soy estudiante de Ingeneria Civil de octavo semestre.</p>
+                </div>
+            </div>
+            <div className="Nosotros_container">
+                <div className="Nosotros_img Nosotros_img--inverse">
+                    <img src={photo2} alt="Laura Rendón" />
+                </div>
+                <div className="Nosotros_main Nosotros_main--inverse">
+                    <h3 className="Nosotros_name">Laura Rendón Noreña - <span><i>Estudiante Ingeniería Civil</i></span></h3>
+                    <p className='Nosotros_paragraph'>Soy estudiante de Ingeneria Civil de octavo semestre.</p>
+                </div>
+            </div>
+            <div className="Nosotros_container">
+                <div className="Nosotros_img">
+                    <img src={photo3} alt="Juan Esteban Velez" />
+                </div>
+                <div className="Nosotros_main">
+                    <h3 className="Nosotros_name">Juan Esteban Vélez Loaiza - <span><i>Estudiante Ingeniería Geológica</i></span></h3>
+                    <p className='Nosotros_paragraph'>Soy estudiante de Ingeneria Geológica de octavo semestre.</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Nosotros
