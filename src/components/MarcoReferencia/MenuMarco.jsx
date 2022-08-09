96ac40ab6b7c5b5a4067163809915d7e27d77edd
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-08-09 10:45:46 -0500

    Completado el cuerpo de información de la Geologia Regional

--- src/components/MarcoReferencia/MenuMarco.jsx
+++ src/components/MarcoReferencia/MenuMarco.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import '../../styles/MenuMarco.css'
 
@@ -22,12 +22,19 @@ const MenuMarco = () => {
     }
   ];
 
+  const handlerClickMarco = e => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  };
+
   return (
     <nav className='MenuMarco_nav'>
       <ul className="MenuMarco_list">
         {caracteristicas.map(caract => (
           <li key={caract.path} className='MenuMarco_item'>
-            <NavLink to={`/marco/${caract.path}`} className={({isActive}) => isActive ? 'MenuMarco_link active' : 'MenuMarco_link'}>{caract.name}</NavLink>
+            <NavLink to={`/marco/${caract.path}`} className={({isActive}) => isActive ? 'MenuMarco_link active' : 'MenuMarco_link'} onClick={handlerClickMarco}>{caract.name}</NavLink>
           </li>
         ))}
       </ul>
